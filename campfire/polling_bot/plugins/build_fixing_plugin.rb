58d1a6e7c21e59ff0f74eac38574eac8e33926fd
Eric Allen <ericpallen@gmail.com>
2010-10-29 12:17:50 -0700

    Lots of bug fixing

--- campfire/polling_bot/plugins/build_fixing_plugin.rb
+++ campfire/polling_bot/plugins/build_fixing_plugin.rb
@@ -10,23 +10,28 @@ class BuildFixingPlugin < Campfire::PollingBot::Plugin
   end
 
   def process(message)
-    if message.body =~ /(.*)\s*fixing the build/
+    return if message.person == bot.name
+    if message.body =~ /who('| i)s .* the build/
+      if @fixers.empty?
+        bot.say("Nobody is fixing the build")
+      else
+        @fixers.each do |fixer|
+          bot.say("#{fixer} is fixing the build")
+        end
+      end
+    elsif message.body =~ /(.*)\s*fixing the build/
       command = $1
       case command
-      when /I'm/
+      when /I'm\s*$/
         @fixers.add(message.person.downcase)
       when /I'm done/
         @fixers.delete(message.person.downcase)
       when /I'm not/
         @fixers.delete(message.person.downcase)
-      when /(.*)\s+ is/
+      when /(.*)\s+is\s*$/
         @fixers.add($1)
-      when /(.*)\s+ is not/
-        @fixers.add($1)
-      when /who.*/
-        @fixers.each do |fixer|
-          bot.say("#{fixer} is fixing the build")
-        end
+      when /(.*)\s+is (not|done)/
+        @fixers.delete($1)
       end
     end
     if message.person == "buildbot" && message.body =~ /FIXED/
